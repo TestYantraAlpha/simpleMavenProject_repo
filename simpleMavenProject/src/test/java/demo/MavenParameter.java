085faa972b611cef0f326e530df06818d7fe4906
Abhijith1998 <123438717+Abhijith1998@users.noreply.github.com>
2024-01-03 12:15:21 +0530

    Removed Test in MavenParameter class

--- simpleMavenProject/src/test/java/demo/MavenParameter.java
+++ simpleMavenProject/src/test/java/demo/MavenParameter.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class MavenParameterTest {
+public class MavenParameter {
 	@Test
 	public void recieveData() {
 		Reporter.log("MavenParameter class executed...",true);
